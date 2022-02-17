e0b63ce3833699d97ec9c22499717055d7276f06
Sandra Juarez <sandjuarezg@gmail.com>
2022-02-17 00:35:05 -0600

    feat: add client

---
+++ client/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sync"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatal("Insufficient arguments: [host] [port]")
+	}
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", os.Args[1], os.Args[2]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	// read server message
+	reply := make([]byte, 1024)
+	_, err = conn.Read(reply)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", reply)
+
+	// read name
+	n, err := bufio.NewReader(os.Stdin).Read(reply)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// write name on connection
+	_, err = conn.Write(reply[:n])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// read server messages
+	n, err = conn.Read(reply)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", reply[:n])
+
+	wg := sync.WaitGroup{}
+
+	// write on connection
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		for {
+			// read name
+			n, err := bufio.NewReader(os.Stdin).Read(reply)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// write message on connection
+			_, err = conn.Write(reply[:n])
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}(&wg)
+
+	// read from connection
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		for {
+			// read messages
+			n, err = conn.Read(reply)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("%s", reply[:n])
+		}
+	}(&wg)
+
+	wg.Wait()
+
+}
